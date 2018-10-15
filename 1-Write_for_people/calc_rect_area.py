d12a8c7f36b30aa58bc628615aabca8bbd1376ea
Ian Cosden <icosden@princeton.edu>
2018-10-15 14:56:02 -0400

    add rectangle calculation example

---
+++ 1-Write_for_people/calc_rect_area.py
@@ -0,0 +1,11 @@
+def calc_rect_area(x1, y1, x2, y2):
+    calculation...
+
+def calc_rect_area(point1, point2):
+    calcultion...
+
+
+surface_area = calc_rect_area(x1, y1, x2, y2)
+
+
+surface_area = calc_rect_area(point2, point1)
