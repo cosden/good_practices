60dd54c55078d7fae735d3b5bad4d2cf0b7c6529
Ian Cosden <icosden@princeton.edu>
2018-10-18 11:51:25 -0400

    updates to calc_rect_area

--- 1-Write_for_people/calc_rect_area.py
+++ 1-Write_for_people/calc_rect_area.py
@@ -1,8 +1,9 @@
 def calc_rect_area(x1, y1, x2, y2):
     calculation...
 
+# caculate rectangle area
 def calc_rect_area(point1, point2):
-    calcultion...
+    calculation...
 
 
 surface_area = calc_rect_area(x1, y1, x2, y2)
